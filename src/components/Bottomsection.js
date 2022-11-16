e5b1962627573e4399a5cf36bfa1dd576a46ae74
mbah kingsley <mbahkingsley9@gmail.com>
2022-11-16 11:07:58 +0100

    font:change color to white

--- src/components/Bottomsection.js
+++ src/components/Bottomsection.js
@@ -5,23 +5,19 @@ import { Button } from 'react-bootstrap';
 const Bottomsection = () => {
   return (
     <div>
-      <div className='container'>
+      <div className='container-fluid'>
         <div style={{backgroundColor:"#a02279",height:"45rem"}} className='row justify-content-around align-items-center'>
           <div className='col-xl-4 col-md-6 col-xs-6 '>
           <h1 className='text-white mb-5'>Metabnb NFTs</h1>
             
-            <p style={{width:"20rem",height:"8.5rem",fontSize:"1.4em",marginBottom:"30px"}}>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.
+            <p style={{width:"20rem",height:"8.5rem",fontSize:"1.4em",marginBottom:"30px"}} className="text-white">Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.
             </p>
             
             <Button style={{backgroundColor:"#ffffff"}} className="border-0 btn-lg mt-5 pt-1"><a style={{color:"#a02279"}} className='text-decoration-none' href="placetostay">Learn more</a></Button>
           </div>
           <div className="col-xl-7 col-md-6 col-xs-6">
             <img style={{width:"550px"}}  className='img-fluid mw-100 h-auto mt-0 ' src={frame2} alt="" />
-            {/* <div className='d-lg-none'>
-              <img style={{width:"rem",height:"6rem"}} className='img-fluid mw-100 h-auto mt-0 ' src={view1} alt="" />
-              <img style={{width:"15rem",height:"6rem"}} className='img-fluid mw-100 h-auto mt-0 ' src={view2} alt="" />
-              
-            </div>   */}
+
           </div>
 
         </div>
