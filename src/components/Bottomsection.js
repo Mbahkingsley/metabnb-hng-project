47a5c856f06cdef7aa72e3da86535935da340817
mbah kingsley <mbahkingsley9@gmail.com>
2022-11-16 13:51:33 +0100

    fixing btn

--- src/components/Bottomsection.js
+++ src/components/Bottomsection.js
@@ -10,7 +10,7 @@ const Bottomsection = () => {
           <div className='col-xl-4 col-md-6 col-xs-6 '>
           <h1 className='text-white mb-5'>Metabnb NFTs</h1>
             
-            <p style={{width:"20rem",height:"8.5rem",fontSize:"1.4em",marginBottom:"30px"}} className="text-white">Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.
+            <p style={{width:"20rem",height:"8.5rem",fontSize:"1.4em",marginBottom:"70px"}} className="text-white">Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.
             </p>
             
             <Button style={{backgroundColor:"#ffffff"}} className="border-0 btn-lg mt-5 pt-1"><a style={{color:"#a02279"}} className='text-decoration-none' href="placetostay">Learn more</a></Button>
